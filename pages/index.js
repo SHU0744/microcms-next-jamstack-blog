9d5fced25d4e7472fba3530f66e322db455ef710
SHU0744 <miyavi.senshu@gmail.com>
2023-12-29 17:41:43 +0900

    タグ

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,27 @@
 import Link from "next/link";
 import { client } from "../libs/client";
 
-export default function Home({ blog, category }) {
+export default function Home({ blog, category, tag }) {
   return (
     <div>
-      <ul>
-        {category.map((category) => (
-          <li key={category.id}>
-            <Link href={`/category/${category.id}`}>{category.name}</Link>
-          </li>
-        ))}
-      </ul>
+      <div>
+        <h5>カテゴリー一覧</h5>
+        <ul>
+          {category.map((category) => (
+            <li key={category.id}>
+              <Link href={`/category/${category.id}`}>{category.name}</Link>
+            </li>
+          ))}
+        </ul>
+        <h5>タグ一覧</h5>
+        <ul>
+          {tag.map((tag) => (
+            <li key={tag.id}>
+              <Link href={`/tag/${tag.id}`}>{tag.name}</Link>
+            </li>
+          ))}
+        </ul>
+      </div>
       <ul>
         {blog.map((blog) => (
           <li key={blog.id}>
@@ -24,13 +35,14 @@ export default function Home({ blog, category }) {
 
 export const getStaticProps = async () => {
   const data = await client.get({ endpoint: "blog" });
-
   const categoryDate = await client.get({ endpoint: "categories" });
+  const tagData = await client.get({ endpoint: "tags" });
 
   return {
     props: {
       blog: data.contents,
       category: categoryDate.contents,
+      tag: tagData.contents,
     },
   };
 };
